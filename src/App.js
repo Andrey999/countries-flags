46ba01c0be6e72b0f6e424a6e67579976334ab00
Andrey999 <gromovandrey1985@gmail.com>
2021-11-07 11:50:01 +0300

    update index

--- src/App.js
+++ src/App.js
@@ -10,33 +10,6 @@ import { NotFound } from './pages/NotFound';
 const App = () => {
   const [countries, setCountries] = useState([]);
 
-
-
-  function colonOdd (num) {
-    let str = num.toString();
-    let result = [str[0]];
-    console.log(result);
-    for(var i=1; i<str.length; i++) {
-        if((str[i-1]%2 !== 0)&&(str[i]%2 !== 0)) {
-          result.push(':', str[i]);
-         }
-        else {
-          result.push(str[i]);
-        }
-    }
-    return result.join('');  
-  }
-
-document.writeln(colonOdd(55639217)); // 5:5
-
-
-
-
-
-
-
-
-
   return (
     <>
       <Header />
